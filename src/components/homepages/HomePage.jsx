0bca69d7eaabac9acf7b5b8d4fe84e4b21a18dbc
yusluv1234 <yusluv124@gamil.com>
2020-12-07 19:16:01 +0100

    first commit

---
+++ src/components/homepages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+
+import './HomePage.scss';
+
+const HomePage = () => {
+    return (
+        <div className='home-page'>
+            <Navbar />
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../icons/logo.svg'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='navbar-logo'>
+
+            </div>
+            <div className='navbar-option'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
