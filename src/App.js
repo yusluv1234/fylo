10e4b78a528c23ee1aadfffe99bb86c8c81737c3
yusluv1234 <yusluv124@gamil.com>
2020-12-07 21:20:00 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
+import HomePage from './components/homepages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      
+      <HomePage />
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Logo from '../../icons/logo.svg'
 
+import './Navbar.scss'
+
 const Navbar = () => {
     return (
         <div className='navbar'>
             <div className='navbar-logo'>
-
+                <a href='/home' alt='option'>
+                <img src={Logo} alt='alt' />
+                </a>
             </div>
             <div className='navbar-option'>
-
+                <a className='href' href='/feature' alt='option'>Features</a>
+                <a className='href' href='/team' alt='option'>Team</a>
+                <a className='href' href='login' alt='option'>Login</a>
             </div>
         </div>
     )
